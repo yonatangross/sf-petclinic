c9b9ce9d4fb666b5ebdc457ab66abd9180374ea1
Yonatan Gross <yonatan2gross@gmail.com>
2019-07-30 10:09:24 +0300

    Initial Project Add

---
+++ src/main/java/yonatangross/spring/sfpetclinic/SfPetclinicApplication.java
@@ -0,0 +1,13 @@
+package yonatangross.spring.sfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfPetclinicApplication.class, args);
+    }
+
+}
