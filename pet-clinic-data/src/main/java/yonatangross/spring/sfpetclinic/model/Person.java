09ef3c627650c9c0ed6a3059dc6acfea8867a8ac
Yonatan Gross <yonatan2gross@gmail.com>
2019-08-05 06:54:48 +0300

    Adding base entity. close #10.

--- pet-clinic-data/src/main/java/yonatangross/spring/sfpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/yonatangross/spring/sfpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package yonatangross.spring.sfpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/yonatangross/spring/sfpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/yonatangross/spring/sfpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package yonatangross.spring.sfpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
